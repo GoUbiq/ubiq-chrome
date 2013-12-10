1830ab7abd214f3fc44d6c3e73167784c5b46189
Sean Stewart <sdstewar@gmail.com>
2013-12-09 23:26:34 -0500

    register device on extension install; load resource on device

--- background.js
+++ background.js
@@ -1,29 +1,25 @@
 var socket = io.connect('http://192.168.0.100:8000/');
-console.log('Connecting to Ubiq server...');
+console.log('Opened socket to Ubiq...');
 
-socket.on('connect', function () {
-    console.log('Connected to Ubiq. Registering device...');
-    
-    var device_data = { 
-        user: 'timmy',
-        device: 'Frinkiac-7'
-    };
-
-    //socket.of('/register').emit('apocalypse', {});
-
-    socket.of('/register').emit('register', device_data);
-
-});
-
-socket.of('/register').on('new_device', function (data) {
-    // TODO: Something
-    console.log(data + ' registered');
+socket.on('error', function () {
+    console.log('Socket error.');
 });
 
 var handleMessage = function (request, sendResponse) {
     switch (request.action) {
         case 'ubiq:device:load-resource':
-            console.log('would load ' + request.resourceURL + ' on device, id = ' + request.deviceID);
+            console.log('Loading ' + request.resourceURL + '...');
+            socket.of('/app').emit('app_start', { params: {
+                app_id: '31337',
+                app_name: 'Ubiq Video'
+            }});
+
+            socket.of('/app').emit('app_action', { params: {
+                app_id: '31337',
+                app_name: 'Ubiq Video',
+                request: 'play_url',
+                url: request.resourceURL
+            }});
             break;
         case 'ubiq:device:list':
             console.log('Requesting device list...');
@@ -39,12 +35,35 @@ var handleMessage = function (request, sendResponse) {
     }
 }
 
+// Browser Events
+// ----------
+chrome.runtime.onInstalled.addListener(function () {
+    // register this device
+    socket.once('connect', function () {
+        console.log('Connected to Ubiq. Registering device...');
+
+        var device_data = {
+            user: 'timmy',
+            device: 'Frinkiac-7'
+        };
+
+        socket.of('/register').emit('register', device_data);
+    });
+
+    socket.of('/register').on('new_device', function (data) {
+        // TODO: Verify shit worked
+        console.log(data + ' registered');
+    });
+
+});
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     // verify socket connection before taking action
     var retry_count = 0;
      
     setTimeout(function () {
         if (retry_count > 5) {
+            console.log('Maximum retries reached. You\'re on your own.');
             sendResponse({ error: 'Unable to connect to server.' });
             return;
         }
